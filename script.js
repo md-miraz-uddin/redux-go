15902bfc4637d04da0e5325185d2583f0692c5a3
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2026-05-04 06:16:35 +0600

    1.1 done

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const counterEl = document.querySelector("#counter")
 
 let count = 0;
 
+// counter incrementing
 incrementEl.addEventListener('click', () => {
     count++;
     counterEl.innerText = count;
 })
 
+// counter decrementing
 decrementEl.addEventListener('click', () => {
+    // counter will not decrement if reached to zero
     if (count > 0) count--;
     counterEl.innerText = count;
 })
